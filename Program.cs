d51f3604abc1e0c65402508d905130f73eee9946
Larsa Tahir <larsatahir@gmail.com>
2026-04-02 23:14:36 +0200

    skapat todolist filen

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ToDoApp;
+
+class Program
+{
+    static void Main()
+    {
+        var todoList = new ToDoList();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("To do lista");
+            Console.WriteLine("1. Visa lista");
+            Console.WriteLine("2. Lägg till ny uppgift");
+            Console.WriteLine("3. Ta bort uppgift");
+            Console.WriteLine("4. Avsluta");
+            Console.WriteLine("Välj: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    ShowTask(todoList);
+                    break;
+
+                case "2":
+                    AddTask(todoList);
+                    break;
+
+                case "3":
+                    RemoveTask(todoList);
+                    break;
+
+                case "4":
+                    running = false;
+                    break;
+
+            }
+        }
+    }
+}
---
+++ TodoList.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace ToDoApp
+{
+    public class ToDoList
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            if (String.IsNullOrWhiteSpace(task))
+            {
+                tasks.Add(task);
+            }
+        }
+
+
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+        public List<string> GetAllTasks()
+        {
+            return new List<string>(tasks);
+        }
+    }
+}
