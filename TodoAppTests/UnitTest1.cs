2611cab50303552da965bad1d08b068c25655418
Larsa Tahir <larsatahir@gmail.com>
2026-04-06 20:44:28 +0200

    skapat lägg till, ta bort och felhanterings tester

--- TodoAppTests/UnitTest1.cs
+++ TodoAppTests/UnitTest1.cs
@@ -1,10 +1,44 @@
-﻿namespace TodoAppTests;
-
-public class UnitTest1
+﻿using Xunit;
+using ToDoApp;
+using System.Collections.Generic;
+using System.Reflection;
+public class ToDoListTests
 {
     [Fact]
-    public void Test1()
+    public void AddTask_ShouldAddTask()
     {
+        var todo = new ToDoList();
+        todo.AddTask("Testuppgift");
 
+        List<string> tasks = todo.GetAllTasks();
+        Assert.Single(tasks);
+        Assert.Equal("Testuppgift", tasks[0]);
+    }
+
+    [Fact]
+    public void RemoveTask_ShouldRemoveTask()
+    {
+        var todo = new ToDoList();
+        todo.AddTask("Test1");
+        todo.AddTask("Test2");
+
+        todo.RemoveTask(0);
+        List<string> tasks = todo.GetAllTasks();
+
+        Assert.Single(tasks);
+        Assert.Equal("Test2", tasks[0]);
+    }
+
+    [Fact]
+    public void RemoveTask_InvalidIndex_ShouldNotThrow()
+    {
+        var todo = new ToDoList();
+        todo.AddTask("Test1");
+
+        todo.RemoveTask(10);
+        List<string> tasks = todo.GetAllTasks();
+
+        Assert.Single(tasks);
+        Assert.Equal("Test1", tasks[0]);
     }
 }
